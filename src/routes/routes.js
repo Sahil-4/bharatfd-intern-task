1a55fdc63e5825e0d9fd0b45dba7329e05c44467
Sahil-4 <www.sahil.875020@gmail.com>
2025-02-03 21:33:40 +0530

    chore: integreted caching

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,6 +9,19 @@ router.get("/faq", async (req, res) => {
   try {
     const { lang = "en" } = req.query;
 
+    // cached response
+    const cacheKey = `faq_${lang}`;
+
+    const cachedFAQs = await redis.get(cacheKey);
+
+    if (cachedFAQs) {
+      return res.status(200).send({
+        success: true,
+        message: "fetched faqs",
+        data: JSON.parse(cachedFAQs),
+      });
+    }
+
     // fetch lang FAQs from db
     const faqs = await Translation.find({ lang });
 
@@ -20,6 +33,9 @@ router.get("/faq", async (req, res) => {
 
     // update the top 5 languages in redis
     await redis.set("top_5_langs", JSON.stringify(topLangs));
+
+    // cache data
+    await redis.setex(cacheKey, 60 * 10, JSON.stringify(faqs));
 
     // return response
     res
@@ -48,6 +64,12 @@ router.post("/faq", async (req, res) => {
 
     // publish "new_faq_added" event
     await redis.publish("new_faq_added", faq._id.toString());
+
+    // invalidate cache
+    const keys = await redis.keys("faq_*");
+    if (keys.length > 0) {
+      await redis.del(...keys);
+    }
 
     // send success response to client
     res.status(200).send({ success: true, message: "added faq", data: [faq] });
